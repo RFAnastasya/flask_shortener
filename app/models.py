5f36276930dc86f01049f4cd74572788c4a1f3f5
rybak.nastasya@gmail.com <rybak.nastasya@gmail.com>
2023-08-08 23:26:12 +0300

    Initial commit

---
+++ app/models.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+from . import db
+
+class URLmodel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    original_url = db.Column(db.String(255))
+    short = db.Column(db.String(6), unique=True)
+    visits = db.Column(db.Integer, default=0)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
